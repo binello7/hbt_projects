578a1befe63a036662977124fb78e50d0fa867a6
Sebastiano Rusca <sebastiano.rusca@gmail.com>
2023-01-30 14:31:06 +0100

    Update paths

--- scripts/richterswil_fw.py
+++ scripts/richterswil_fw.py
@@ -6,8 +6,9 @@ from hbt_tools import fremdwasser as fw
 
 # Paths
 data_dir = Path(r"C:\Users\sru\OneDrive - Hunziker Betatech AG"
-    r"\Documents\04_Entwicklung\data\01_Rohdaten")
-save_dir = Path(r"C:\Users\sru\Downloads\hist")
+    r"\Documents\04_Entwicklung\data\Richterswil")
+save_dir = Path(r"C:\Users\sru\OneDrive - Hunziker Betatech AG\Documents"
+    r"\04_Entwicklung\outputs")
 path_ara = data_dir / "Zulaufmenge_ARA_Skript_1min.csv"
 
 # Read data
@@ -18,4 +19,5 @@ ts_ara = pd.read_csv(path_ara, sep=';', index_col=0, parse_dates=[0],
 # Filter data
 ts_ara = ts_ara[(ts_ara>=20) & (ts_ara<=150)]
 
-fig = fw.hist_flow(ts_ara=ts_ara, save_dir=save_dir)
+# Plot histogram
+fig = fw.hist_flow(ts_ara=ts_ara, save_dir=save_dir, save_name='hist.png')
